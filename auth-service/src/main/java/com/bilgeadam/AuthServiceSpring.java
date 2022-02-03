c37728c8ff853d710869d60f6f1892a71d74dd52
MuhammetAli <muhammedali55@gmail.com>
2022-02-03 16:58:17 +0300

    auth service add

--- auth-service/src/main/java/com/bilgeadam/AuthServiceSpring.java
+++ auth-service/src/main/java/com/bilgeadam/AuthServiceSpring.java
@@ -1,13 +1,12 @@
 package com.bilgeadam;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceSpring {
+public class AuthServiceSpring {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceSpring.class);
+        SpringApplication.run(AuthServiceSpring.class);
     }
 }
