4690fee523b49a568710cf6c41fe52315a5d76f7
MuhammetAli <muhammedali55@gmail.com>
2022-02-03 16:41:59 +0300

    user gradle

---
+++ user-service/src/main/java/com/bilgeadam/UserServiceSpring.java
@@ -0,0 +1,11 @@
+package com.bilgeadam;
+
+
+import java.util.Arrays;
+
+public class UserServiceSpring {
+
+    public static void main(String[] args) {
+        System.out.println("args = " + Arrays.deepToString(args));
+    }
+}
