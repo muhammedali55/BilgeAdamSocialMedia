8994096e8a8e96797d2368445f7ead1d82b9d553
MuhammetAli <muhammedali55@gmail.com>
2022-02-04 11:56:45 +0300

    User Controller ayzıldı.

---
+++ auth-service/src/main/java/com/bilgeadam/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.bilgeadam.controller;
+
+import com.bilgeadam.dto.request.RegisterRequestDto;
+import com.bilgeadam.repository.entity.User;
+import com.bilgeadam.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    // ReturnType
+    // -> returnCode-> error->9XXX -> 9001-> username and password error
+    // -> success-> 1XXX -> 1000, 1100
+    // Burada validasyon yapılmalı.
+    @PostMapping("/dologin")
+    public ResponseEntity<User> doLogin(String username,String password){
+        Optional<User> user = userService.findByUsernameAndPassword(username, password);
+        if (user.isPresent())
+            return ResponseEntity.ok(user.get());
+        return ResponseEntity.ok(new User());
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@RequestBody @Valid RegisterRequestDto dto){
+        // 1. Etapta-> Auth içni kayıt olmalı
+        userService.saveReturnUser(User.builder()
+                        .username(dto.getEmail())
+                        .password(dto.getSifre())
+                .build());
+        // 2. Etapta-> User-Service e kayıt için istek atmalı, dönen cevaba göre işle devam etmeli.
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/findall")
+    public ResponseEntity<List<User>> findAll(){
+        return ResponseEntity.ok(userService.findAll());
+    }
+
+
+}
--- auth-service/src/main/java/com/bilgeadam/service/UserService.java
+++ auth-service/src/main/java/com/bilgeadam/service/UserService.java
@@ -4,6 +4,9 @@ import com.bilgeadam.repository.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserService {
 
@@ -11,7 +14,7 @@ public class UserService {
     IUserRepository iUserRepository;
 
     /**
-     * Kullanıcıyı kayıt eder
+     * Kullanıcıyı kayıt eder ve kayıtedilen kullanıcının id bilgisi alınarak geri döndürülür.
      * @param user
      * @return
      */
@@ -22,6 +25,29 @@ public class UserService {
 
     public void save(User user){
         iUserRepository.save(user);
+    }
+
+    public void update(User user){
+        iUserRepository.save(user);
+    }
+
+    public void delete(User user){
+        iUserRepository.delete(user);
+    }
+
+    public List<User> findAll(){
+        return iUserRepository.findAll();
+    }
+
+    public Optional<User> findByUsernameAndPassword(String username,String password){
+        return iUserRepository.findByUsernameAndPassword(username, password);
+    }
+
+    public boolean isUser(String username,String password){
+       Optional<User> user = iUserRepository.findByUsernameAndPassword(username, password);
+       if (user.isPresent())
+           return true;
+        return false;
     }
 
 
