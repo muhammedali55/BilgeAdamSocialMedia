723fcf5258c5bc8684638c200635ef9fba6e0d38
MuhammetAli <muhammedali55@gmail.com>
2022-02-11 11:25:51 +0300

    Login html eklendi.

---
+++ web-service/src/main/java/com/bilgeadam/WebService.java
@@ -0,0 +1,12 @@
+package com.bilgeadam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebService.class, args);
+    }
+}
