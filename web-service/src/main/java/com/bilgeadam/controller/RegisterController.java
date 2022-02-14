754b1619abcce892ee57527f8fa499396782d4f1
MuhammetAli <muhammedali55@gmail.com>
2022-02-14 12:05:59 +0300

    Login Register Ayrıldı. Bulut sunucuya bağlantılar yapıldı.

--- web-service/src/main/java/com/bilgeadam/controller/RegisterController.java
+++ web-service/src/main/java/com/bilgeadam/controller/RegisterController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
@@ -24,6 +25,12 @@ public class RegisterController {
     private final S3ManagerService s3ManagerService;
 
     private final AuthServiceManager authServiceManager;
+
+    //http:localhost/register
+    @GetMapping("/register")
+    public ModelAndView register() {
+        return new ModelAndView("register");
+    }
 
     @PostMapping("/register")
     public Object register(String ad, String soyad,
