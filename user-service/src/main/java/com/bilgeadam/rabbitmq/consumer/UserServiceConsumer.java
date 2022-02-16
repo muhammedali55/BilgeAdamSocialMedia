322e09b766863466a45573121c33fd814234c80d
MuhammetAli <muhammedali55@gmail.com>
2022-02-16 12:45:52 +0300

    Queue Bildirim göndermek Producer-Consumer Ok.

---
+++ user-service/src/main/java/com/bilgeadam/rabbitmq/consumer/UserServiceConsumer.java
@@ -0,0 +1,16 @@
+package com.bilgeadam.rabbitmq.consumer;
+
+import com.bilgeadam.rabbitmq.model.Notification;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UserServiceConsumer {
+
+    @RabbitListener(queues = "queueCreateUser")
+    public void consumeNotification(Notification notification){
+       log.info("Bildirim geldi....: "+ notification.toString());
+    }
+}
